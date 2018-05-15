e14cf399800a16708dd64a85d21fc8cad57366ac
V <V@mail.ru>
2018-05-16 00:41:31 +0300

    headers

--- Client/Program.cs
+++ Client/Program.cs
@@ -10,24 +10,31 @@ namespace Client
         {
             var fileName = "File.mp4";
 
-            var buffer = new byte[1024];
+            var buffer = new byte[4096];
 
             using (var file = File.Open(fileName, FileMode.Append))
             using (var client = new WebClient())
             using (var stream = client.OpenRead($"http://localhost:54680/api/v1/Files/{Guid.NewGuid()}?seek={file.Length}"))
             {
+                foreach (string responseHeadersAllKey in client.ResponseHeaders.AllKeys)
+                {
+                  Console.WriteLine($"{responseHeadersAllKey} : {client.ResponseHeaders[responseHeadersAllKey]}"); 
+                }
+
                 while (stream.CanRead)
                 {
-                    var countReaded = stream.Read(buffer, 0, buffer.Length);
+                    var countRead = stream.Read(buffer, 0, buffer.Length);
 
-                    if (countReaded < 1)
+                    if (countRead < 1)
                         break;
 
-                    file.Write(buffer, 0, countReaded);
+                    file.Write(buffer, 0, countRead);
 
                     file.Flush();
                 }
             }
+
+            Console.ReadLine();
         }
     }
 }
--- src/Server/Controllers/FilesController.cs
+++ src/Server/Controllers/FilesController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.IO;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using Microsoft.Net.Http.Headers;
 
 namespace Server.Controllers
 {
@@ -11,22 +13,31 @@ namespace Server.Controllers
         [HttpGet("{id}")]
         public IActionResult Get(Guid id, long seek)
         {
+            var path = Path.Combine(
+                Directory.GetCurrentDirectory(),
+                "wwwroot",
+                "videos",
+                "Video.mp4"
+                );
+
+            var fileInfo = new FileInfo(path);
+
+            if (seek < 0 || seek > fileInfo.Length)
+                throw new ArgumentOutOfRangeException(nameof(seek));
+
+            if (seek == fileInfo.Length)
+                return File(new byte[0], "application/octet-stream", "Video.mp4", new DateTimeOffset(fileInfo.LastWriteTimeUtc), null);
+
             var stream = new FileStream(
-                Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "videos", "Video.mp4"),
+                path,
                 FileMode.Open,
                 FileAccess.Read,
                 FileShare.Read
                 );
 
-            if (seek < 0 || seek > stream.Length)
-                throw new ArgumentOutOfRangeException(nameof(seek));
-
-            if (seek == stream.Length)
-                return File(new byte[0], "application/octet-stream");
-
             stream.Seek(seek, SeekOrigin.Begin);
 
-            return File(stream, "application/octet-stream");
+            return File(stream, "application/octet-stream", "Video.mp4", new DateTimeOffset(fileInfo.LastWriteTimeUtc), null);
         }
 
         // POST api/values
