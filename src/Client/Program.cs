6d385e68066dce75c1b3b0b34beb625a9018a6a1
V <V@mail.ru>
2018-05-16 23:24:07 +0300

    added uploading files

---
+++ src/Client/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileName = "File.mp4";
+            Download(fileName);
+            Upload(fileName);
+        }
+
+        static void Download(string id)
+        {
+            using (var fileStream = File.Open(id, FileMode.Append))
+            using (var client = new WebClient())
+            using (var serverStream = client.OpenRead($"http://localhost:54680/api/v1/Files/{id}?seek={fileStream.Length}"))
+            {
+                Copy(serverStream, fileStream);
+            }
+        }
+
+        static void Upload(string id)
+        {
+            using (var fileStream = File.Open(id, FileMode.Open))
+            using (var client = new WebClient())
+            using (var serverStream = client.OpenWrite($"http://localhost:54680/api/v1/Files/{id}?append=false", "PUT"))
+            {
+                var size = long.Parse(client.DownloadString("http://localhost:54680/api/v1/Files/{id}/Size"));
+                fileStream.Seek(size, SeekOrigin.Begin);
+                Copy(fileStream, serverStream);
+            }
+        }
+
+        static long Copy(Stream from, Stream to)
+        {
+            long copiedByteCount = 0;
+            byte[] buffer = new byte[4096];
+            var len = 0;
+            while (from.CanRead && (len = from.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                to.Write(buffer, 0, len);
+                to.Flush();
+                copiedByteCount += len;
+            }
+            return copiedByteCount;
+        }
+    }
+}
--- src/Server/Controllers/FilesController.cs
+++ src/Server/Controllers/FilesController.cs
@@ -11,51 +11,61 @@ namespace Server.Controllers
     {
         // GET api/values
         [HttpGet("{id}")]
-        public IActionResult Get(Guid id, long seek)
+        public IActionResult Get(string id, long seek)
         {
-            var path = Path.Combine(
-                Directory.GetCurrentDirectory(),
-                "wwwroot",
-                "videos",
-                "Video.mp4"
-                );
-
+            var path = GetFilePath(id);
             var fileInfo = new FileInfo(path);
-
             if (seek < 0 || seek > fileInfo.Length)
                 throw new ArgumentOutOfRangeException(nameof(seek));
-
             if (seek == fileInfo.Length)
-                return File(new byte[0], "application/octet-stream", "Video.mp4", new DateTimeOffset(fileInfo.LastWriteTimeUtc), null);
-
+                return File(new byte[0], "application/octet-stream", id, new DateTimeOffset(fileInfo.LastWriteTimeUtc), null);
             var stream = new FileStream(
                 path,
                 FileMode.Open,
                 FileAccess.Read,
                 FileShare.Read
                 );
-
             stream.Seek(seek, SeekOrigin.Begin);
-
-            return File(stream, "application/octet-stream", "Video.mp4", new DateTimeOffset(fileInfo.LastWriteTimeUtc), null);
+            return File(stream, "application/octet-stream", id, new DateTimeOffset(fileInfo.LastWriteTimeUtc), null);
         }
 
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public IActionResult Put(string id, bool append = false)
         {
+            var path = GetFilePath(id);
+            if (!append && System.IO.File.Exists(path))
+                System.IO.File.Delete(path);
+            using (var stream = append ? System.IO.File.Open(path, FileMode.Append) : System.IO.File.Create(path))
+            {
+                Copy(Request.Body, stream);
+            }
+            return Ok();
         }
 
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        [HttpGet("{id}/[action]")]
+        public long Size(string id)
         {
+            var fileInfo = new FileInfo(GetFilePath(id));
+            return fileInfo.Exists ? fileInfo.Length : 0;
+        }
+
+        private static string GetFilePath(string id)
+        {
+            return Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "videos", id);
+        }
+
+        private static long Copy(Stream from, Stream to)
+        {
+            long copiedByteCount = 0;
+            byte[] buffer = new byte[2 << 16];
+            var len = 0;
+            while (from.CanRead && (len = from.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                to.Write(buffer, 0, len);
+                to.Flush();
+                copiedByteCount += len;
+            }
+            return copiedByteCount;
         }
     }
 }
