16310a453fc6288119bd8e78523f1b1d2fe1c288
V <V@mail.ru>
2018-05-17 00:09:54 +0300

    refactoring

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -4,34 +4,34 @@ using System.Net;
 
 namespace Client
 {
-    class Program
+    public class Client
     {
-        static void Main(string[] args)
-        {
-            var fileName = "File.mp4";
-            Download(fileName);
-            Upload(fileName);
-        }
-
-        static void Download(string id)
+        public static void Download(string id)
         {
             using (var fileStream = File.Open(id, FileMode.Append))
             using (var client = new WebClient())
             using (var serverStream = client.OpenRead($"http://localhost:54680/api/v1/Files/{id}?seek={fileStream.Length}"))
             {
                 Copy(serverStream, fileStream);
+                fileStream.Close();
             }
         }
 
-        static void Upload(string id)
+        public static void Upload(string id)
         {
             using (var fileStream = File.Open(id, FileMode.Open))
             using (var client = new WebClient())
             using (var serverStream = client.OpenWrite($"http://localhost:54680/api/v1/Files/{id}?append=false", "PUT"))
             {
                 var size = long.Parse(client.DownloadString("http://localhost:54680/api/v1/Files/{id}/Size"));
+                if (size >= fileStream.Length)
+                    return;
+                client.Headers.Add("Content-Type", "application/octet-stream");
+                client.Headers.Add("Content-Disposition", "attachment; filename=" + id);
+                client.Headers.Add("Content-Length", (fileStream.Length - size).ToString());
                 fileStream.Seek(size, SeekOrigin.Begin);
                 Copy(fileStream, serverStream);
+                fileStream.Close();
             }
         }
 
@@ -40,13 +40,23 @@ namespace Client
             long copiedByteCount = 0;
             byte[] buffer = new byte[4096];
             var len = 0;
-            while (from.CanRead && (len = from.Read(buffer, 0, buffer.Length)) > 0)
+            while (from.CanRead && to.CanWrite && (len = from.Read(buffer, 0, buffer.Length)) > 0)
             {
                 to.Write(buffer, 0, len);
                 to.Flush();
                 copiedByteCount += len;
             }
             return copiedByteCount;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileName = "File.mp4";
+            Client.Download(fileName);
+            Client.Upload(fileName);
         }
     }
 }
--- src/Server/Controllers/FilesController.cs
+++ src/Server/Controllers/FilesController.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.IO;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Primitives;
-using Microsoft.Net.Http.Headers;
 
 namespace Server.Controllers
 {
@@ -11,35 +9,41 @@ namespace Server.Controllers
     {
         // GET api/values
         [HttpGet("{id}")]
-        public IActionResult Get(string id, long seek)
+        public void Get(string id, long seek)
         {
             var path = GetFilePath(id);
             var fileInfo = new FileInfo(path);
             if (seek < 0 || seek > fileInfo.Length)
                 throw new ArgumentOutOfRangeException(nameof(seek));
             if (seek == fileInfo.Length)
-                return File(new byte[0], "application/octet-stream", id, new DateTimeOffset(fileInfo.LastWriteTimeUtc), null);
-            var stream = new FileStream(
+                return;
+            HttpContext.Response.ContentType = "application/octet-stream";
+            HttpContext.Response.Headers.Add("Content-Disposition", "attachment; filename=" + id);
+            HttpContext.Response.Headers.Add("Content-Length", (fileInfo.Length - seek).ToString());
+            using (var fileStream = new FileStream(
                 path,
                 FileMode.Open,
                 FileAccess.Read,
                 FileShare.Read
-                );
-            stream.Seek(seek, SeekOrigin.Begin);
-            return File(stream, "application/octet-stream", id, new DateTimeOffset(fileInfo.LastWriteTimeUtc), null);
+            ))
+            {
+                fileStream.Seek(seek, SeekOrigin.Begin);
+                Copy(fileStream, HttpContext.Response.Body);
+                fileStream.Close();
+            }
         }
 
         [HttpPut("{id}")]
-        public IActionResult Put(string id, bool append = false)
+        public void Put(string id, bool append = false)
         {
             var path = GetFilePath(id);
             if (!append && System.IO.File.Exists(path))
                 System.IO.File.Delete(path);
-            using (var stream = append ? System.IO.File.Open(path, FileMode.Append) : System.IO.File.Create(path))
+            using (var fileStream = append ? System.IO.File.Open(path, FileMode.Append) : System.IO.File.Create(path))
             {
-                Copy(Request.Body, stream);
+                Copy(Request.Body, fileStream);
+                fileStream.Close();
             }
-            return Ok();
         }
 
         [HttpGet("{id}/[action]")]
@@ -59,7 +63,7 @@ namespace Server.Controllers
             long copiedByteCount = 0;
             byte[] buffer = new byte[2 << 16];
             var len = 0;
-            while (from.CanRead && (len = from.Read(buffer, 0, buffer.Length)) > 0)
+            while (from.CanRead && to.CanWrite && (len = from.Read(buffer, 0, buffer.Length)) > 0)
             {
                 to.Write(buffer, 0, len);
                 to.Flush();
